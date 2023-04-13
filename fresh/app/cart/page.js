020999d19d9ced889113e1cb6f8a1d1665d3b135
halamlee <halam.lee.lami@gmail.com>
2023-04-14 04:38:41 +0900

    refactor: CartItem 컴포넌트화

--- fresh/app/cart/page.js
+++ fresh/app/cart/page.js
@@ -1,17 +1,22 @@
 export default function Cart() {
+  let cartItems = ['Tomatoes', 'Pasta'];
+
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+      {cartItems.map((item) => {
+        return <CartItem item={item} />;
+      })}
+    </div>
+  );
+}
+
+function CartItem(props) {
+  return (
+    <div className="cart-item">
+      <p>{props.item}</p>
+      <p>$40</p>
+      <p>1개</p>
     </div>
   );
 }
