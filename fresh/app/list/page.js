e924eaba06c5d4a8a652693d50155994241d095b
halamlee <halam.lee.lami@gmail.com>
2023-04-14 06:22:21 +0900

    feat: 장바구니 수량 조절 기능 추가

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,5 +1,10 @@
+'use client';
+
+import { useState } from 'react';
+
 export default function List() {
   let products = ['Tomatoes', 'Pasta', 'Coconut'];
+  const [count, setCount] = useState([0, 0, 0]);
 
   return (
     <div>
@@ -9,6 +14,23 @@ export default function List() {
           <div className="food" key={idx}>
             <img src={`/food${idx}.png`} className="food-img" />
             <h4>{item} $40</h4>
+            <button
+              onClick={() => {
+                let copy = [...count];
+                copy[idx]--;
+                setCount(copy);
+              }}>
+              -
+            </button>
+            <span> {count[idx]} </span>
+            <button
+              onClick={() => {
+                let copy = [...count];
+                copy[idx]++;
+                setCount(copy);
+              }}>
+              +
+            </button>
           </div>
         );
       })}
