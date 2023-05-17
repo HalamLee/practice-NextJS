8bd72b15399d02bf2efcf666338cfad53d5a3afc
Lami <Halam.Lee.Lami@gmail.com>
2023-05-18 02:08:00 +0900

    design: index 페이지 스타일링

--- fast-campus/blog/pages/index.jsx
+++ fast-campus/blog/pages/index.jsx
@@ -3,6 +3,8 @@ import Layout, { siteTitle } from '../components/Layout';
 import utilStyles from '../styles/utils.module.css';
 // import { getSortedPostsData } from '../lib/posts'; => CSR 방식에서는 사용하지 못함.
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Date from '../components/date';
 
 // SSG로 렌더링하기
 // export async function getStaticProps() {
@@ -79,11 +81,11 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
