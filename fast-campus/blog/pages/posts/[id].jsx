a56b5765811b881be4ad09e275b1b4c791f996fc
Lami <Halam.Lee.Lami@gmail.com>
2023-05-18 02:01:03 +0900

    design: post 페이지 스타일 추가

--- fast-campus/blog/pages/posts/[id].jsx
+++ fast-campus/blog/pages/posts/[id].jsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Layout from '../../components/Layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -23,13 +24,14 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      <Date dateString={postData.date} />
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
