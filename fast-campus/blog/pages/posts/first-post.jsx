1c040891592480e5f87c1fe46c896da0cb71d306
Lami <Halam.Lee.Lami@gmail.com>
2023-05-17 12:24:19 +0900

    feat: polishing layout

--- fast-campus/blog/pages/posts/first-post.jsx
+++ fast-campus/blog/pages/posts/first-post.jsx
@@ -1,3 +1,15 @@
+import Head from 'next/head';
+import Layout from '../../components/Layout';
+
 export default function FirstPost() {
-  return <h1>첫번째 글</h1>;
+  return (
+    <>
+      <Head>
+        <title>첫번째 글</title>
+      </Head>
+      <Layout>
+        <h1>첫번째 글</h1>
+      </Layout>
+    </>
+  );
 }
